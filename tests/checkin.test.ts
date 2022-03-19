c599410c013635e937dcc283dc172004ebd91f73
vaaski <admin@vaa.ski>
2022-03-19 14:30:24 +0100

    refactor(test): make checkin test suite more concise

--- tests/checkin.test.ts
+++ tests/checkin.test.ts
@@ -15,6 +15,7 @@ before(async () => {
 beforeEach(delay)
 
 let checkin: Checkin.CheckinResponse
+let checkinList: Checkin.CheckinList
 
 after(async () => {
   try {
@@ -25,64 +26,45 @@ after(async () => {
 })
 
 test("get checkin list", async t => {
-  const data = await instance.checkin.list()
-  t.true(data.checkins instanceof Array)
-  t.truthy(data.checkins[0].firstname)
-  t.true(typeof data.checkins[0].databaseId === "number")
-})
-
-test("get checkin list with a unitID", async t => {
-  const data = await instance.checkin.list({
-    organizationUnitId: TEST_FACILITY,
-  })
-  t.true(data.checkins instanceof Array)
-  t.truthy(data.checkins[0].firstname)
-  t.true(typeof data.checkins[0].databaseId === "number")
+  checkinList = await instance.checkin.list()
+  t.true(checkinList.checkins instanceof Array)
+  t.truthy(checkinList.checkins[0].firstname)
+  t.true(typeof checkinList.checkins[0].databaseId === "number")
 })
 
 test("check-in a customer", async t => {
-  const data = await instance.checkin.checkin({
-    lockerKey: "openmagicline automated test",
-    fkCustomer: TEST_CUSTOMER,
-  })
-  t.true(data.fkCustomer === TEST_CUSTOMER)
-})
-
-test("check-out a customer", async t => {
-  const checkin = await instance.checkin.checkin({
-    lockerKey: "openmagicline automated test",
-    fkCustomer: TEST_CUSTOMER,
-    requiredOrganizationUnitId: TEST_FACILITY,
-  })
-
-  const checkout = await instance.checkin.checkout(checkin.databaseId)
-  t.true(checkout.fkCustomer === TEST_CUSTOMER)
-})
-
-test("change lockerKey part 1: ensure checkin", async t => {
-  const lockerKey = "openmagicline automated test"
-
   checkin = await instance.checkin.checkin({
-    lockerKey,
+    lockerKey: "openmagicline automated test",
     fkCustomer: TEST_CUSTOMER,
-    fkOrganizationUnit: TEST_FACILITY,
   })
-
-  t.assert(checkin.lockerKey === lockerKey)
+  t.true(checkin.fkCustomer === TEST_CUSTOMER)
 })
 
 const changedTo = "openmagicline automated test 2"
-test("change lockerKey part 2: change key", async t => {
+test("change lockerKey: change key", async t => {
   const changed = await instance.checkin.lockerKey(checkin.databaseId, changedTo)
 
   t.assert(changed.lockerKey === changedTo)
   t.assert(changed.checkinId === checkin.databaseId)
 })
 
-test("change lockerKey part 3: check key", async t => {
-  const list = await instance.checkin.list()
-  const changedCheckin = list.checkins.find(c => c.databaseId === checkin.databaseId)
+test("get checkin list with a unitID", async t => {
+  checkinList = await instance.checkin.list({
+    organizationUnitId: TEST_FACILITY,
+  })
+  t.true(checkinList.checkins instanceof Array)
+  t.truthy(checkinList.checkins[0].firstname)
+  t.true(typeof checkinList.checkins[0].databaseId === "number")
+})
+
+test("change lockerKey: check key", async t => {
+  const changedCheckin = checkinList.checkins.find(c => c.databaseId === checkin.databaseId)
   if (!changedCheckin) throw "can't find lockerkey-changed checkin anymore"
 
   t.true(changedCheckin.lockerKey === changedTo)
+})
+
+test("check-out a customer", async t => {
+  const checkout = await instance.checkin.checkout(checkin.databaseId)
+  t.true(checkout.fkCustomer === TEST_CUSTOMER)
 })
